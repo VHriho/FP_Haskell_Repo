3e30d409f0ee2860c83c7b08d1d7e1e965f9d3c0
VHriho <v12grig12@gmail.com>
2024-06-17 18:39:07 +0300

    solved Problem 2

--- FP-PS-Task1/main.hs
+++ FP-PS-Task1/main.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 {- PROBLEM 1
 It is often needed to convert a number written in arabic symbols,
 to a string of its textual representation, i.e. for financial documents.
@@ -26,6 +28,27 @@ problem1 = do
     print $ intToWords    0  -- "zero"
     print $ intToWords (-10) -- "minus-one-zero"
 
+{- PROBLEM 2
+Write a function findMaxFrequency that for a given homogenous list of type a
+returns a pair (a, Int) of the most frequent element (any, if there are more than one) and its frequecy. For an empty list throw an error. 
+-}
+
+findMaxFrequency :: (Ord a) => [a] -> (a, Int)
+findMaxFrequency [] = error "empty list"
+findMaxFrequency lst = myTranspose (maximum (foldr step [] (group (sort lst))))
+    where step x ys = (length x, head x) : ys
+
+myTranspose :: (b, a) -> (a, b)
+myTranspose (x, y) = (y, x) 
+
+problem2 = do
+    print "Problem 2"
+    print $ findMaxFrequency [1,2,1,3,1,4]   -- (1, 3)
+    print $ findMaxFrequency [1,1,2,2]       -- (1, 2) or (2, 2)
+    print $ findMaxFrequency "some sentence" -- ('e', 4)
+    -- print $ findMaxFrequency ([] :: String)  -- error
+
 main = do
-    problem1    
+    problem1
+    problem2    
 

ee8a50f220180dfc5b93350261d85aea58bf9696
VHriho <v12grig12@gmail.com>
2024-06-17 19:05:03 +0300

    2 Naive implementation with insert and empty

--- FP-PS-Task2/main.hs
+++ FP-PS-Task2/main.hs
@@ -23,14 +23,27 @@ empty        :: Dict k v
 --    d denotes the actual type of the dictionary (its type constructor)   
 class IDict d k v where
     insert       :: k -> v -> d k v -> d k v
-    maybeGet     :: k -> d k v -> Maybe v
+    -- maybeGet     :: k -> d k v -> Maybe v
     -- getOrDefault :: k -> d k v -> v -> v
-    contains     :: k -> d k v -> Bool
-    delete       :: k -> d k v -> d k v
-    elems        :: d k v -> [v]
-    keys         :: d k v -> [k]
-    size         :: d k v -> Int
+    -- contains     :: k -> d k v -> Bool
+    -- delete       :: k -> d k v -> d k v
+    -- elems        :: d k v -> [v]
+    -- keys         :: d k v -> [k]
+    -- size         :: d k v -> Int
     empty        :: d k v 
 
+-- 2. Propose a naive implementation of the typeclass above 
+data Dict k v = Dict [(k, v)]
+
+instance IDict Dict Int Char where
+    insert :: Int -> Char -> Dict Int Char -> Dict Int Char
+    insert k v (Dict dict) = Dict (helpInsert k v dict) where 
+        helpInsert k v [] = [(k, v)]
+        helpInsert k v ((x, y):xs)| k == x    = (k, v) : xs 
+                                | otherwise = (x, y) : helpInsert k v xs
+
+    empty :: Dict Int Char
+    empty = Dict []
+
 main = do
-    print "1. Populated typeclass"
+    print "naive implementation of the typeclass"
